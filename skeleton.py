d4d9e3773b25bcc655b92b899f0fed21afb2858b
Nick Groenen <zoni@zoni.nl>
2012-12-30 16:07:32 +0100

    Correct tiny docstring bug

--- skeleton.py
+++ skeleton.py
@@ -4,7 +4,7 @@ from errbot import BotPlugin, botcmd
 from errbot.builtins.webserver import webhook
 
 class Skeleton(BotPlugin):
-	""""An Err plugin skeleton"""
+	"""An Err plugin skeleton"""
 	min_err_version = '1.6.0' # Optional, but recommended
 	max_err_version = '1.7.1' # Optional, but recommended
 

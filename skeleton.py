errbotio/plugin-skeleton

commit af07a01e90e3a1c99e4fe2921a6fa7d6d8d61a58

Remove example code, make things more bare-bones

skeleton.py
@@ -3,6 +3,7 @@
 from errbot import BotPlugin, botcmd
 from errbot.builtins.webserver import webhook
 
+
 class Skeleton(BotPlugin):
 	"""An Err plugin skeleton"""
 	min_err_version = '1.6.0' # Optional, but recommended
@@ -10,40 +11,56 @@ class Skeleton(BotPlugin):
 
 	def activate(self):
 		"""Triggers on plugin activation
-		
-		In this case, starts a poller that triggers each second
-		"""
+
+		You should delete it if you're not using it to override any default behaviour"""
 		super(Skeleton, self).activate()
 
-		# Make sure our shelf has a counter. Putting this check here saves
-		# an if statement being executed once every second (in the poller)
-		if 'counter' not in self.shelf.keys():
-			self.shelf['counter'] = 0
-		self.start_poller(1, self.poll)
+	def deactivate(self):
+		"""Triggers on plugin deactivation
+
+		You should delete it if you're not using it to override any default behaviour"""
+		super(Skeleton, self).deactivate()
 
 	def get_configuration_template(self):
-		"""Defines the configuration structure this plugin supports"""
+		"""Defines the configuration structure this plugin supports
+
+		You should delete it if your plugin doesn't use any configuration like this"""
 		return {'EXAMPLE_KEY_1': "Example value",
 		        'EXAMPLE_KEY_2': ["Example", "Value"]
 		       }
 
-	def poll(self):
-		"""An example poller which increments a simple counter"""
-		self.shelf['counter'] += 1
+	def check_configuration(self, configuration):
+		"""Triggers when the configuration is checked, shortly before activation
+
+		You should delete it if you're not using it to override any default behaviour"""
+		super(Skeleton, self).check_configuration()
+
+	def callback_connect(self):
+		"""Triggers when bot is connected
+
+		You should delete it if you're not using it to override any default behaviour"""
+		pass
+
+	def callback_message(self, conn, message):
+		"""Triggered for every received message that isn't coming from the bot itself
+
+		You should delete it if you're not using it to override any default behaviour"""
+		pass
+
+	def callback_botmessage(self, conn, message):
+		"""Triggered for every message that comes from the bot itself
+
+		You should delete it if you're not using it to override any default behaviour"""
+		pass
 
 	# Passing split_args_with=None will cause arguments to be split on any kind
 	# of whitespace, just like Python's split() does
 	@botcmd(split_args_with=None)
-	def counter_show(self, mess, args):
-		"""Return the number of counter ticks since reset"""
-		return "My counter has ticked {0} times since being reset".format(self.shelf['counter'])
-
-	@botcmd(split_args_with=None)
-	def counter_reset(self, mess, args):
-		"""Reset the counter"""
-		self.shelf['counter'] = 0
-		return "Counter reset"
-
 	@webhook
-	def counter(self, incoming_request):
-		return "My counter has ticked {0} times since being reset\n".format(self.shelf['counter'])
+	def example(self, incoming_request):
+		"""An example command
+
+		Simply returns the string "Example" and is available as a webhook on /example/
+		as well (assuming the 'Webserver' plugin is correctly configured).
+		"""
+		return "Example"

commit 85f01613b84facdb6a219ead53cc2e401e92da31

Fix mistakes introduced by af07a01e90e3a1c99e4fe2921a6fa7d6d8d61a58

skeleton.py
@@ -47,20 +47,20 @@ class Skeleton(BotPlugin):
 		You should delete it if you're not using it to override any default behaviour"""
 		pass
 
-	def callback_botmessage(self, conn, message):
+	def callback_botmessage(self, message):
 		"""Triggered for every message that comes from the bot itself
 
 		You should delete it if you're not using it to override any default behaviour"""
 		pass
 
+	@webhook
+	def example_webhook(self, incoming_request):
+		"""A webhook which simply returns 'Example'"""
+		return "Example"
+
 	# Passing split_args_with=None will cause arguments to be split on any kind
 	# of whitespace, just like Python's split() does
 	@botcmd(split_args_with=None)
-	@webhook
-	def example(self, incoming_request):
-		"""An example command
-
-		Simply returns the string "Example" and is available as a webhook on /example/
-		as well (assuming the 'Webserver' plugin is correctly configured).
-		"""
+	def example(self, mess, args):
+		"""A command which simply returns 'Example'"""
 		return "Example"
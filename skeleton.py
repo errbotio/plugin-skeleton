90823f22a624de66ade4c775b7b79982f37a30db
Nick Groenen <nick@groenen.me>
2016-03-12 18:28:17 +0100

    Update skeleton.py

--- skeleton.py
+++ skeleton.py
@@ -1,55 +1,67 @@
-# This is a skeleton for Err plugins, use this to get started quickly.
-
 from errbot import BotPlugin, botcmd, webhook
 
 
 class Skeleton(BotPlugin):
-    """An Err plugin skeleton"""
-    min_err_version = '1.6.0' # Optional, but recommended
-    max_err_version = '2.0.0' # Optional, but recommended
+    """
+    Fill in your plugin description here.
+    """
 
     def activate(self):
-        """Triggers on plugin activation
+        """
+        Triggers on plugin activation
 
-        You should delete it if you're not using it to override any default behaviour"""
+        You should delete it if you're not using it to override any default behaviour
+        """
         super(Skeleton, self).activate()
 
     def deactivate(self):
-        """Triggers on plugin deactivation
+        """
+        Triggers on plugin deactivation
 
-        You should delete it if you're not using it to override any default behaviour"""
+        You should delete it if you're not using it to override any default behaviour
+        """
         super(Skeleton, self).deactivate()
 
     def get_configuration_template(self):
-        """Defines the configuration structure this plugin supports
+        """
+        Defines the configuration structure this plugin supports
 
-        You should delete it if your plugin doesn't use any configuration like this"""
+        You should delete it if your plugin doesn't use any configuration like this
+        """
         return {'EXAMPLE_KEY_1': "Example value",
                 'EXAMPLE_KEY_2': ["Example", "Value"]
                }
 
     def check_configuration(self, configuration):
-        """Triggers when the configuration is checked, shortly before activation
+        """
+        Triggers when the configuration is checked, shortly before activation
 
-        You should delete it if you're not using it to override any default behaviour"""
+        You should delete it if you're not using it to override any default behaviour
+        """
         super(Skeleton, self).check_configuration()
 
     def callback_connect(self):
-        """Triggers when bot is connected
+        """
+        Triggers when bot is connected
 
-        You should delete it if you're not using it to override any default behaviour"""
+        You should delete it if you're not using it to override any default behaviour
+        """
         pass
 
     def callback_message(self, message):
-        """Triggered for every received message that isn't coming from the bot itself
+        """
+        Triggered for every received message that isn't coming from the bot itself
 
-        You should delete it if you're not using it to override any default behaviour"""
+        You should delete it if you're not using it to override any default behaviour
+        """
         pass
 
     def callback_botmessage(self, message):
-        """Triggered for every message that comes from the bot itself
+        """
+        Triggered for every message that comes from the bot itself
 
-        You should delete it if you're not using it to override any default behaviour"""
+        You should delete it if you're not using it to override any default behaviour
+        """
         pass
 
     @webhook
